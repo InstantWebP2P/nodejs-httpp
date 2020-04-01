fbec7a6194aa18885d8b20c1d4f6ff08f4b4a404
tom zhou <iwebpp@gmail.com>
2020-03-31 19:24:38 -0700

    code cleanup

--- example.js
+++ example.js
@@ -6,14 +6,14 @@ var httpps = require('httpps');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello, iWebPP:\n\tThis is Tom calling to Jagua from TCP.:)\n');
+  response.end('Hello, AppNet:\n\tThis is Tom calling to Jagua from TCP.:)\n');
 }).listen(3008);
 
 console.log('Server running at http://127.0.0.1:3008/');
 
 httpp.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello, iWebPP:\n\tThis is Tom calling to Jagua from UDP.:)\n');
+  response.end('Hello, AppNet:\n\tThis is Tom calling to Jagua from UDP.:)\n');
 }).listen(3008);
 
 console.log('Server running at httpp://127.0.0.1:3008/');
